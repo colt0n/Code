5c68501c9faa192703920159402b324472a97363
colt0n <67947437+colt0n@users.noreply.github.com>
2025-02-10 11:15:56 +0800

    Delete data.py

--- data.py
+++
@@ -1,5 +0,0 @@
-import tushare as ts
-ts.set_token('667c4753bc057f8ddeda9fab5e5ed97f0bd0ef5e954ceb4ae9296795')
-pro = ts.pro_api()
-df = pro.daily(ts_code='000001.SZ', start_date='20180701', end_date='20180718')
-print(df)
